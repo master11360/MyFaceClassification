2ad04dd5b88ab21b22c92bff5d1223e764e38d2d
Yueh-Ming Chien <master113600@gmail.com>
2017-02-26 01:30:39 +0800

    Add Item class and use it to control images and its class
    1. There are 5 attributes in Item: img, class_num, file_name, file_path
    and normalized_img. Use them to keep loaded item.
    2. In main, use items instead of some image list. It's more
    object-oriented and easy to read.
    3. Read 2 items from img path: training items and target items. Use
    training items to fit SVM and target items to predict.

---
+++ item.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+class Item:
+    def __init__(self, img, class_num, file_name, file_path):
+        self.__img = img
+        self.__class_Num = class_num
+        self.__file_name = file_name
+        self.__file_path = file_path
+        self.__normalized_img = None
+
+    @property
+    def img(self):
+        return self.__img
+
+    @img.setter
+    def img(self, img):
+        self.__img = img
+
+    @property
+    def class_num(self):
+        return self.__class_Num
+
+    @class_num.setter
+    def class_num(self, class_num):
+        self.__class_Num = class_num
+
+    @property
+    def normalized_img(self):
+        return self.__normalized_img
+
+    @normalized_img.setter
+    def normalized_img(self, normalized_img):
+        self.__normalized_img = normalized_img
+
+    def __str__(self):
+        return 'file_name = ' + self.__file_name + ', class_num = ' + str(self.__class_Num) + ', file_path = ' \
+               + self.__file_path
--- main.py
+++ main.py
@@ -2,8 +2,17 @@
 import cv2
 import json
 import os
+from sklearn import svm
+from sklearn import metrics
+import numpy as np
+from item import Item
+import matplotlib.pyplot as plt
 
-DEBUG = False
+DEBUG = True
+DEBUG_TRAINING_IMG = False
+DEBUG_TARGET_IMG = False
+KEY_TRAINING_PATH = 'training_path'
+KEY_TARGET_PATH = 'target_path'
 VALID_IMG_TYPE = ('.jpg', '.jpeg', '.png')
 NORMALIZED_SIZE = (128, 128)
 
@@ -14,21 +23,55 @@ def main():
         return
 
     print '[main] Config:', config_json
-    training_imgs = load_images(config_json['training_path'])
-    if len(training_imgs) == 0:
+
+    training_items = load_images(config_json[KEY_TRAINING_PATH])
+    target_items = load_images(config_json[KEY_TARGET_PATH])
+    if len(training_items) == 0 or len(target_items) == 0:
         print '[main] No images in training path!'
         return
 
-    print '[main]', len(training_imgs), 'training images are read'
-    normalized_training_imgs = resize_images(training_imgs)
-    print '[main]', len(training_imgs), 'training images are normalized'
-
     if DEBUG:
-        for i, f in enumerate(normalized_training_imgs):
-            cv2.imshow('training'+str(i), f)
+        print '[main] training items ----------------------------'
+        for item in training_items:
+            print item
+        print '[main] target items ----------------------------'
+        for item in target_items:
+            print item
 
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    print '[main]', len(training_items), 'training images, ', len(target_items), 'target images are read'
+    resize_images(training_items)
+    print '[main]', len(training_items), 'training images are normalized, size =', NORMALIZED_SIZE
+    resize_images(target_items)
+    print '[main]', len(target_items), 'target images are normalized, size =', NORMALIZED_SIZE
+
+    if DEBUG_TRAINING_IMG:
+        show_images(training_items)
+    if DEBUG_TARGET_IMG:
+        show_images(target_items)
+
+    # Create a classifier: a support vector classifier
+    classifier = svm.SVC(gamma=0.001)
+    training_imgs = [np.array(item.normalized_img).flatten().tolist() for item in training_items]
+    training_class_num = [item.class_num for item in training_items]
+    classifier.fit(training_imgs, training_class_num)
+
+    # Now predict the value of the digit on the second half:
+    target_imgs = [np.array(item.normalized_img).flatten().tolist() for item in target_items]
+    target_class_num = [item.class_num for item in target_items]
+    predicted = classifier.predict(target_imgs)
+
+    print("Classification report for classifier %s:\n%s\n"
+          % (classifier, metrics.classification_report(target_class_num, predicted)))
+    print("Confusion matrix:\n%s" % metrics.confusion_matrix(target_class_num, predicted))
+
+    # images_and_predictions = list(zip(training_imgs, predicted))
+    # for index, (image, prediction) in enumerate(images_and_predictions[:2]):
+    #     plt.subplot(1, 2, index+1)
+    #     plt.axis('off')
+    #     plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    #     plt.title('Prediction: %i' % prediction)
+    #
+    # plt.show()
 
 
 def load_json_file(path):
@@ -42,25 +85,47 @@ def load_json_file(path):
 
 
 def is_valid_config_file(config_file):
-    if config_file and config_file['training_path'] and config_file['target_path']:
+    if config_file and config_file[KEY_TRAINING_PATH] and config_file[KEY_TARGET_PATH]:
         return True
     else:
-        print '[is_valid_config_file] Can\'t find your img path in config.'
-        print '[is_valid_config_file] Please set attribute \'path\' for it.'
+        if not config_file[KEY_TRAINING_PATH]:
+            print '[is_valid_config_file] Can\'t find your training img path in config.'
+            print '[is_valid_config_file] Please set attribute \'training_path\' for it.'
+        elif not config_file[KEY_TARGET_PATH]:
+            print '[is_valid_config_file] Can\'t find your target img path in config.'
+            print '[is_valid_config_file] Please set attribute \'target_path\' for it.'
         return False
 
 
 def load_images(img_path):
-    imgs = []
-    for f in os.listdir(img_path):
-        if os.path.splitext(f)[1].lower() in VALID_IMG_TYPE:
-            imgs.append(cv2.imread(os.path.join(img_path, f)))
-    return imgs
+    items = []
+    for file in os.listdir(img_path):
+        file_split_text = os.path.splitext(file)
+        file_name = file_split_text[0]
+        file_ext = file_split_text[1]
+        if file_ext.lower() in VALID_IMG_TYPE:
+            file_path = os.path.join(img_path, file)
+            img = cv2.imread(file_path)
+            class_num = int(file_name.split('_')[0])
+            item = Item(img, class_num, file, file_path)
+            items.append(item)
+    return items
 
 
-def resize_images(imgs):
-    return [cv2.resize(img, NORMALIZED_SIZE) for img in imgs]
+def resize_images(items):
+    for item in items:
+        item.normalized_img = cv2.resize(item.img, NORMALIZED_SIZE)
+
+
+def show_images(items):
+    imgs = [item.normalized_img for item in items]
+    for i, f in enumerate(imgs):
+        cv2.imshow('training img ' + str(i), f)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
     main()
+    # main_test()
